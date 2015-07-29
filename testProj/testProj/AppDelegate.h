2358910417b664b6be6c83723eaa26c9f96a1136
sabrinatuli <Btrac@noc1-pc.btraccl.com>
2015-07-29 12:54:53 +0600

    initial test commit

---
+++ testProj/testProj/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testProj
+//
+//  Created by Btrac on 7/29/15.
+//  Copyright (c) 2015 Btraccl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
